e5099dc7ac1334eabdb4e8bb33cf41faf5939e42
Matheus <theusantana123@hotmail.com>
2025-02-18 10:15:01 -0300

    feat: adding migration for tags

---
+++ src/CashFlow.Domain/Entities/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Domain.Entities;
+public class Tag
+{
+	public long Id { get; set; }
+	public Enums.Tag Value { get; set; } //Foi colocado o diretório anterior 'Enums.' para não confundir com o nome da Classe Tag e dar conflito.
+	public long ExpenseId { get; set; } 
+	public Expense Expense { get; set; } = default!;
+}
